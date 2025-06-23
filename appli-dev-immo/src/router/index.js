5c0ebc2e4526fb8a86589a61207ea6b17d281f7f
unknown <diallocoumbiss676@gmail.com>
2025-06-23 22:54:57 +0000

    Nettoyage projet vue.js , configuration router , suppression des fichiers temporaires

---
+++ appli-dev-immo/src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+//mpotation des pages de views
+
+const routes = [
+    
+ //chemin des pages
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
